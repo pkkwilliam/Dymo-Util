6e7a423112cf3e0cd678830b3772f475067c3d58
pkkwilliam <pkkwilliam919@gmail.com>
2020-11-17 16:14:02 +0800

    make tcovert this to npm package

---
+++ dist/print.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// must include this in the index.html header, add DYMO.Label.Framework.3.0.js in same path as index file
+
+{
+  /* <title>React App</title>
+  <script
+  src="DYMO.Label.Framework.3.0.js"
+  type="text/javascript"
+  charset="UTF-8"
+  ></script> */
+}
+
+function print(label) {
+  var printerName = "";
+  for (var i = 0; i < printers.length; ++i) {
+    var printer = printers[i];
+    console.log(printer.name);
+    if (printer.printerType == "LabelWriterPrinter") {
+      printerName = printer.name;
+      break;
+    }
+  }
+  label.print(printerName);
+}
