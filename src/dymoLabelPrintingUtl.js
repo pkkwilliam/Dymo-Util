6b0371012faf6c8d8abc44044d9b2b54d5c2cee3
pkkwilliam <pkkwilliam919@gmail.com>
2020-11-18 17:25:20 +0800

    ready

--- src/dymoLabelPrintingUtl.js
+++ src/dymoLabelPrintingUtl.js
@@ -37,44 +37,4 @@ function printLabel(labelXml, data) {
   );
 }
 
-function getAddressLabelXml() {
-  var labelXml =
-    '<?xml version="1.0" encoding="utf-8"?>\
-                            <DieCutLabel Version="8.0" Units="twips">\
-                                <PaperOrientation>Landscape</PaperOrientation>\
-                                <Id>Address</Id>\
-                                <PaperName>30252 Address</PaperName>\
-                                <DrawCommands>\
-                                    <RoundRectangle X="0" Y="0" Width="1581" Height="5040" Rx="270" Ry="270" />\
-                                </DrawCommands>\
-                                <ObjectInfo>\
-                                    <AddressObject>\
-                                        <Name>Address</Name>\
-                                        <ForeColor Alpha="255" Red="0" Green="0" Blue="0" />\
-                                        <BackColor Alpha="0" Red="255" Green="255" Blue="255" />\
-                                        <LinkedObjectName></LinkedObjectName>\
-                                        <Rotation>Rotation0</Rotation>\
-                                        <IsMirrored>False</IsMirrored>\
-                                        <IsVariable>True</IsVariable>\
-                                        <HorizontalAlignment>Left</HorizontalAlignment>\
-                                        <VerticalAlignment>Middle</VerticalAlignment>\
-                                        <TextFitMode>ShrinkToFit</TextFitMode>\
-                                        <UseFullFontHeight>True</UseFullFontHeight>\
-                                        <Verticalized>False</Verticalized>\
-                                        <StyledText/>\
-                                        <ShowBarcodeFor9DigitZipOnly>False</ShowBarcodeFor9DigitZipOnly>\
-                                        <BarcodePosition>BelowAddress</BarcodePosition>\
-                                        <LineFonts/>\
-                                    </AddressObject>\
-                                    <Bounds X="332" Y="150" Width="4455" Height="1260" />\
-                                </ObjectInfo>\
-                            </DieCutLabel>';
-  return labelXml;
-}
-
-const data = [{ key: "Address", value: "!@#$%^&*(*&^%$#@#$%^&*" }];
-
-function printThis() {
-  console.log("嘿嘿");
-  printLabel(getAddressLabelXml(), data);
-}
+export default printLabel;
